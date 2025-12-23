84ab94064f2170dcc391f3690bbae9e25c4ab930
fabiandiaz416 <diazfabian99@icloud.com>
2025-12-22 22:51:43 -0300

    feat match for tomorrow 23 december 10 am

--- contract/script/CreateFutbolTenisTomorrow.s.sol
+++ contract/script/CreateFutbolTenisTomorrow.s.sol
@@ -8,22 +8,18 @@ interface IBetterPlay {
     external returns (uint256 id);
 }
 
-contract CreateTestMarket is Script {
-  uint96 constant DEFAULT_FEE_BPS = 200; // 2%
+contract CreateFutbolTenisTomorrow is Script {
+  uint96 constant DEFAULT_FEE_BPS = 200;
 
   function run() external {
     address betterplay = vm.envAddress("BETTERPLAY");
     address stakeToken = vm.envAddress("STAKE_TOKEN");
     uint96 feeBps = uint96(vm.envOr("FEE_BPS", uint256(DEFAULT_FEE_BPS)));
 
-    // siempre en el futuro (30 min)
-    uint64 closeTime = uint64(block.timestamp + 30 minutes);
+    // Cierra apuestas: 2025-12-23 10:00 AR
+    uint64 closeTime = 1766494800;
 
-    // metadata para identificar el test
-    string memory meta = string.concat(
-      "bp://test-futboltenis-fabi-lucho-",
-      vm.toString(uint256(block.timestamp))
-    );
+    string memory meta = "bp://2025-12-23-fabi-vs-lucho-futboltenis";
 
     vm.startBroadcast();
     IBetterPlay(betterplay).openMarket(stakeToken, closeTime, feeBps, meta);
